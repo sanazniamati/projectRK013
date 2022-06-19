c18f5006b0e54cd38b0a01932c8488010eadd7a0
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-06-19 13:30:53 +0430

    first

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, TextPath } from "react-konva";
 
 const App = () => {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Rect
-          x={20}
-          y={20}
-          width={100}
-          height={50}
-          fill="green"
-          stroke="black"
-          strokWidth={4}
-        />
-        <Rect
-          x={150}
-          y={40}
-          width={100}
-          height={50}
-          fill="red"
-          shadowBlur={10}
-          stroke="black"
-          cornerRadius={10}
-        />
-        <Rect
-          x={50}
-          y={120}
-          width={100}
-          height={100}
-          fill="blue"
-          cornerRadius={[0, 10, 20, 30]}
+        <TextPath
+          x={100}
+          y={50}
+          fill={"#333"}
+          fontSize={16}
+          fontFamily={"Arial"}
+          text={`All the world's a stage, and all the men and women merely players.`}
+          data={"M10,10 C0,0 10,150 100,100 S300,150 5.0.300"}
         />
       </Layer>
     </Stage>
